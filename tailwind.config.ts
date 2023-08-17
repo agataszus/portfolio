7fa56698478a206838eb34d637208fd6e106b847
Agata Szustkiewicz <agataszus@gmail.com>
2023-08-17 12:37:14 +0200

    feat(config): added prettier and husky

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,30 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      mobile: { max: "767px" },
+      tablet: { max: "1023px" },
+      "desktop-small": { max: "1365px" },
+    },
+    fontFamily: {
+      main: "Poppins",
+    },
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        primary: "#95FAFE",
+        "background-color-dark": "#0D0E38",
+        "background-color-light": "#293868",
+        "font-color-light": "#FFFFFF",
       },
     },
   },
   plugins: [],
-}
-export default config
+};
+
+export default config;
