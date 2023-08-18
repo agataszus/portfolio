0896f6d42d0c3af1054d57520b4d42ea2d76bb8b
Agata Szustkiewicz <agataszus@gmail.com>
2023-08-18 11:51:22 +0200

    feat(config): added eslint config

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,5 +4,9 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function HomePage() {
-  return <div className="sm:flex-row flex flex-col items-center hover:text-primary mobile:flex">Home Page</div>;
+  return (
+    <div className="sm:flex-row mb-10 flex flex-col items-center justify-between hover:text-primary mobile:flex">
+      Home Page
+    </div>
+  );
 }
