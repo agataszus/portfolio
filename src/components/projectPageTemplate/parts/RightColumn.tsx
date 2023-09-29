5858780f05ea5d37eb9a24d3071c68d0d95987ae
Agata Szustkiewicz <agataszus@gmail.com>
2023-09-29 10:45:42 +0200

    feat(tooltip): added tooltip to technology icon in prject page

--- src/components/projectPageTemplate/parts/RightColumn.tsx
+++ src/components/projectPageTemplate/parts/RightColumn.tsx
@@ -1,48 +1,52 @@
 import { Text } from "@/components/text/Text";
 import Image from "next/image";
 import PlayLineIcon from "remixicon-react/PlayLineIcon";
-import { ProjectTechIcon, TechnologyName } from "./ProjectTechIcon";
+import { TechnologyName } from "./ProjectTechIcon";
 import { motion } from "framer-motion";
+import { ProjectIconContainer } from "./ProjectIconContainer";
+
+const IMAGE_SCALE = 0.8;
+const SUBHEADING_OFFSET = 150;
 
 type RightColumnProps = {
   imageLink: string;
-  technologies: readonly TechnologyName[];
+  technologies: readonly { name: TechnologyName; description: string }[];
 };
 
 export const RightColumn = ({ imageLink, technologies }: RightColumnProps) => {
   return (
-    <div className="z-10 flex flex-col items-end gap-16">
+    <div className="z-10 flex flex-col items-end gap-16 [--hover-opacity:0]">
       <motion.button
         className="relative h-[210] w-[400] bg-black shadow-xl"
-        initial={{ opacity: 0, scale: 0.8 }}
-        animate={{ opacity: [0, 1], scale: [0.8, 1] }}
+        initial={{ opacity: 0, scale: IMAGE_SCALE }}
+        animate={{ opacity: [0, 1], scale: [IMAGE_SCALE, 1] }}
         transition={{ delay: 0.2, duration: 0.5, type: "spring", stiffness: 80 }}
       >
         <Image alt="Project website screen" src={imageLink} width={400} height={210} />
         <PlayLineIcon className="absolute left-1/2 top-1/2 h-20 w-20 -translate-x-1/2 -translate-y-1/2 fill-gray-100/30" />
       </motion.button>
-      <div className="flex flex-col items-end gap-8 overflow-hidden">
-        <motion.div
-          initial={{ x: 150 }}
-          animate={{ x: [150, 0] }}
-          transition={{ delay: 0.3, duration: 0.35, ease: "easeOut" }}
-        >
-          <Text tag="h4" variant="subtitle-2" className="italic text-white">
-            technologies
-          </Text>
-        </motion.div>
+      <div className="flex flex-col items-end gap-8">
+        <div className="overflow-hidden">
+          <motion.div
+            initial={{ x: SUBHEADING_OFFSET }}
+            animate={{ x: [SUBHEADING_OFFSET, 0] }}
+            transition={{ delay: 0.3, duration: 0.35, ease: "easeOut" }}
+          >
+            <Text tag="h4" variant="subtitle-2" className="italic text-white">
+              technologies
+            </Text>
+          </motion.div>
+        </div>
         <div className="flex w-[300px] flex-wrap justify-end gap-4">
           {technologies.length === 0
             ? null
-            : technologies.map((technology, index) => (
-                <motion.div
-                  key={technology}
-                  initial={{ rotate: "90deg", opacity: 0 }}
-                  animate={{ rotate: ["-110deg", "0deg"], opacity: [0, 1] }}
-                  transition={{ delay: 0.5 + index / 10, duration: 0.4 }}
-                >
-                  <ProjectTechIcon technologyName={technology} />
-                </motion.div>
+            : technologies.map(({ name, description }, index) => (
+                <ProjectIconContainer
+                  technologyName={name}
+                  description={description}
+                  additionalDelay={index / 10}
+                  key={name}
+                />
               ))}
         </div>
       </div>
--- src/services/content/getProjects.ts
+++ src/services/content/getProjects.ts
@@ -5,7 +5,7 @@ const PROJECTS = [
     slug: "eventhive",
     name: "EventHive",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, consequuntur sunt. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi possimus iusto.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi possimus iusto.",
     iconName: IconNames.SPARKLES_ICON,
     TechnologyIcon: "react",
     subtitle: "Vite + React project",
@@ -14,22 +14,49 @@ const PROJECTS = [
     demoLink: "https://eventhive-sigma.vercel.app/",
     websiteScreen: "/",
     technologies: [
-      "react",
-      "vite",
-      "typescript",
-      "css-modules",
-      "sass",
-      "tanstack-query",
-      "vercel",
-      "eslint",
-      "prettier",
+      {
+        name: "react",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      },
+      {
+        name: "vite",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "typescript",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "css-modules",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "sass",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "tanstack-query",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "vercel",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "eslint",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "prettier",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
     ],
   },
   {
     slug: "portfolio",
     name: "Portfolio",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, consequuntur sunt. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi.",
     iconName: IconNames.PORTFOLIO_ICON,
     TechnologyIcon: "next",
     subtitle: "Vite + Next.js project",
@@ -37,13 +64,46 @@ const PROJECTS = [
     sourceCodeLink: "https://github.com/agataszus/portfolio",
     demoLink: "https://portfolio-agataszus.vercel.app/",
     websiteScreen: "/",
-    technologies: ["next", "typescript", "tailwind", "framer-motion", "eslint", "prettier", "graphql", "datocms"],
+    technologies: [
+      {
+        name: "next",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "typescript",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "tailwind",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "framer-motion",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "eslint",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "prettier",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "graphql",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "datocms",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
   },
   {
     slug: "weather-app",
     name: "WeatherApp",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, consequuntur sunt. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi possimus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi possimus.",
     iconName: IconNames.WEATHER_ICON,
     TechnologyIcon: "javascript",
     subtitle: "Javascript project",
@@ -51,13 +111,30 @@ const PROJECTS = [
     sourceCodeLink: "https://github.com/agataszus/weather-app",
     demoLink: "https://taupe-stardust-ee5f74.netlify.app/",
     websiteScreen: "/",
-    technologies: ["javascript", "vite", "eslint", "css-modules"],
+    technologies: [
+      {
+        name: "javascript",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "vite",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "eslint",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "css-modules",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
   },
   {
     slug: "efdance",
     name: "EFDance",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, consequuntur sunt. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi.",
     iconName: IconNames.MUSIC_ICON,
     TechnologyIcon: "html",
     subtitle: "HTML5 project",
@@ -65,13 +142,26 @@ const PROJECTS = [
     sourceCodeLink: "https://github.com/agataszus/efdance-website",
     demoLink: "https://agataszus.github.io/efdance-website/",
     websiteScreen: "/",
-    technologies: ["css3", "html5", "javascript"],
+    technologies: [
+      {
+        name: "css3",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "html5",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+      {
+        name: "javascript",
+        description: "Lorem ipsum dolor sit amet consectetur adipisicing elit.",
+      },
+    ],
   },
   {
     slug: "algorithms",
     name: "Algorithms",
     description:
-      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem, consequuntur sunt. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi possimus.",
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Nemo soluta omnis, tenetur aperiam nihil, saepe commodi possimus.",
     iconName: IconNames.SORT_ICON,
     TechnologyIcon: "next",
     subtitle: "Vite + Next.js project",
