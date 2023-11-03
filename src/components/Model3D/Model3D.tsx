169052cabbc8292abd71a5658d51520f9682dffd
Agata Szustkiewicz <agataszus@gmail.com>
2023-11-03 16:00:07 +0100

    feat(model3D): added animations to the model and adjusted orbit controls config

--- src/components/Model3D/Model3D.tsx
+++ src/components/Model3D/Model3D.tsx
@@ -2,9 +2,11 @@ import { Canvas, useLoader } from "@react-three/fiber";
 import { Mesh } from "./parts/Mesh";
 import { OrbitControls } from "@react-three/drei";
 import { OBJLoader } from "three/examples/jsm/Addons.js";
+import { Vector3 } from "three";
 
 export const Model3D = () => {
-  const react3D = useLoader(OBJLoader, "/icons3D/react3.obj");
+  const react3D = useLoader(OBJLoader, "/icons3D/react.obj");
+  const vector0 = new Vector3(0, 0, 0);
 
   return (
     <div className="absolute inset-0 h-[200%] w-[200%] -translate-x-1/4 translate-y-[-35%] desktop-small:h-[150%] desktop-small:w-[150%] desktop-small:translate-x-[-20%] desktop-small:translate-y-[-25%]">
@@ -30,11 +32,19 @@ export const Model3D = () => {
         <group rotation={[0.26, -0.1, 0.1, "XYZ"]}>
           <Mesh angle={80} radius={1.9} speed={0.007} src="/icons3D/next.obj" />
           <Mesh angle={170} radius={2.2} speed={0.008} src="/icons3D/html.obj" />
-          <Mesh angle={290} radius={1.5} speed={0.011} src="/icons3D/javascript2.obj" />
+          <Mesh angle={290} radius={1.5} speed={0.011} src="/icons3D/javascript.obj" />
           <Mesh angle={120} radius={2.4} speed={0.007} src="/icons3D/css.obj" />
-          <Mesh angle={310} radius={2.9} speed={0.007} src="/icons3D/typescript3.obj" />
+          <Mesh angle={310} radius={2.9} speed={0.007} src="/icons3D/typescript.obj" />
         </group>
-        <OrbitControls />
+        <OrbitControls
+          minDistance={4.5}
+          maxDistance={6}
+          target0={vector0}
+          minAzimuthAngle={-Math.PI / 4}
+          maxAzimuthAngle={Math.PI / 4}
+          minPolarAngle={(Math.PI * 4) / 9}
+          maxPolarAngle={(Math.PI * 5) / 9}
+        />
       </Canvas>
     </div>
   );
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,6 +4,7 @@ import { Topbar } from "@/components/topbar/Topbar";
 import { useScrollToTopOnRender } from "@/hooks/useScrollToTopOnRender";
 import { DESKTOP, DESKTOP_MID, DESKTOP_SMALL, TABLET, useMediaQueries } from "@/hooks/useMediaQueries";
 import { Model3DMobile } from "@/components/model3DMobile/Model3DMobile";
+import { motion } from "framer-motion";
 
 export default function HomePage() {
   useScrollToTopOnRender();
@@ -16,10 +17,15 @@ export default function HomePage() {
         <div className="w-1/2">
           <Hero />
         </div>
-        <div className="relative w-1/2 tablet:h-[400px]">
+        <motion.div
+          className="relative w-1/2 tablet:h-[400px]"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: [0, 1] }}
+          transition={{ delay: 0.5, duration: 1 }}
+        >
           {[DESKTOP, DESKTOP_MID, DESKTOP_SMALL].includes(mediaQuery) && <Model3D />}
           {mediaQuery === TABLET && <Model3DMobile />}
-        </div>
+        </motion.div>
       </div>
     </div>
   );
